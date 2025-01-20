895d75f57a224d59dc3d7df879a9bb56fe267d8a
a1-bit <ayadav23198@gmail.com>
2025-01-20 17:25:34 +0530

    add size for video and image

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 
 let imageCount = 0;
 let videoCount = 0;
+let imageSize = 0
+let videoSize = 0
 document.getElementById("image_count").textContent = imageCount;
 document.getElementById("video_count").textContent = videoCount;
 function getImgData(input) {
@@ -9,12 +11,15 @@ function getImgData(input) {
         const videoContainer = document.getElementById("video_container");
 
         Array.from(input.files).forEach((file, index) => {
+            console.log('file:', file)
             const fileType = file.type.split("/")[0];
             const url = URL.createObjectURL(file);
 
             const mediaWrapper = document.createElement("div");
             mediaWrapper.classList.add("relative", "inline-block");
             const mediaName = document.createElement("span");
+            const sizeTab = document.createElement('div')
+            sizeTab.classList.add('text-sm', 'font-blod')
             mediaName.classList.add("text-sm", "font-bold");
             const cancelButton = document.createElement("button");
             cancelButton.textContent = "X";
@@ -35,18 +40,22 @@ function getImgData(input) {
                 "cursor-pointer"
             );
             cancelButton.onclick = () => {
-                console.log(fileType, "LL");
                 if (fileType == "video") {
                     document.getElementById("video_count").textContent = --videoCount;
+                    videoSize = videoSize - file.size
+                    document.getElementById("video_size").textContent = getSizeFromBytes(videoSize)
                 }
                 if (fileType == "image") {
                     document.getElementById("image_count").textContent = --imageCount;
+                    imageSize = imageSize - file.size
+                    document.getElementById("image_size").textContent = getSizeFromBytes(imageSize)
                 }
                 URL.revokeObjectURL(url);
                 mediaWrapper.remove();
             };
             if (fileType === "image") {
                 imageCount++;
+                imageSize = imageSize + file.size
                 const newImg = document.createElement("img");
                 newImg.src = url;
                 newImg.alt = `Uploaded image ${index + 1}`;
@@ -63,6 +72,7 @@ function getImgData(input) {
                 mediaWrapper.appendChild(newImg);
             } else if (fileType === "video") {
                 videoCount++;
+                videoSize = videoSize + file.size
                 const newVideo = document.createElement("video");
                 newVideo.src = url;
                 newVideo.controls = true;
@@ -78,18 +88,37 @@ function getImgData(input) {
                 );
                 mediaWrapper.appendChild(newVideo);
             }
-            mediaName.textContent = file.name;
+            sizeTab.textContent = getSizeFromBytes(file.size);
+            mediaName.textContent = truncateStr(file.name);
             mediaWrapper.appendChild(cancelButton);
             mediaWrapper.appendChild(mediaName);
+            mediaWrapper.appendChild(sizeTab)
             if (fileType === "image") {
+                console.log('imageSize:', imageSize)
                 document.getElementById("image_count").textContent = imageCount;
+                document.getElementById("image_size").textContent = getSizeFromBytes(imageSize)
                 imageContainer.appendChild(mediaWrapper);
             } else if (fileType === "video") {
                 document.getElementById("video_count").textContent = videoCount;
+                document.getElementById('video_size').textContent = getSizeFromBytes(videoSize)
                 videoContainer.appendChild(mediaWrapper);
+
             }
         });
     }
+}
+
+function getSizeFromBytes(bytes) {
+    const units = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    let size = bytes;
+    let index = 0;
+
+    while (size > 1024 && index < units.length - 1) {
+        size /= 1024;
+        index++;
+    }
+
+    return size.toFixed(3) + " " + units[index];
 }
 
 async function handleUpload() {
@@ -102,4 +131,14 @@ async function handleUpload() {
             "Content-Type": "application/json"
         },
     })
+}
+
+function truncateStr(name) {
+    if (name.length > 15) {
+        let start = name.substr(0, 7)
+        let end = name.substr(-10)
+        return start + "....." + end
+    }
+    return name
+
 }
